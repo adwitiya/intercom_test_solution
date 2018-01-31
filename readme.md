dc2a3f74f06999f915f7de5a922c315f97112f51
adwitiya <chakraad@tcd.ie>
2018-01-31 03:10:52 +0000

    added tabulate pretty print added comments minor changes

--- readme.md
+++ readme.md
@@ -1,2 +1,3 @@
 Calcualate distance between two locations using great circle distance formula.
-Used Pandas Library and Math Library
+Used Pandas Library and Math Library.
+Used Tabulate Library to pretty print the output
--- solution.py
+++ solution.py
@@ -7,6 +7,9 @@
 
 import pandas as pd
 from math import sin, cos, sqrt, atan2, radians
+from tabulate import tabulate
+
+# Global Constants
 INTERCOM_LONG =  -6.257664
 INTERCOM_LAT = 53.339428
 RADIUS_EARTH = 6373.0 #Approx Radius of earth in KMs
@@ -20,30 +23,40 @@ def read_file(file_name,lines):
     file_contents = pd.read_json(file_name, lines=lines)
     return file_contents
 
+# Calculate the Distance between points
 def calculate_distance(file_contents):
     invite = []
     for index, row in file_contents.iterrows():
+        # Needs to be converted into radians
         diff_long = radians(INTERCOM_LONG) - radians(row['longitude'])
         diff_lat = radians(INTERCOM_LAT) - radians(row['latitude'])
 
+        # Great Circle Distance Formula
         a = sin(diff_lat / 2) ** 2 + cos(row['latitude']) * cos(INTERCOM_LAT) * sin(diff_long / 2) ** 2
         c = 2 * atan2(sqrt(a), sqrt(1 - a))
 
         distance = RADIUS_EARTH * c
-
+        # Creating a new feature based on invite selection criteria
         if distance <= 100:
             invite.append('yes')
         else:
             invite.append('no')
+            
+    # Appending the newly created feature to the dataframe
     file_contents['invite'] = invite
 
 def main():
     url = 'https://gist.githubusercontent.com/brianw/19896c50afa89ad4dec3/raw/6c11047887a03483c50017c1d451667fd62a53ca/gistfile1.txt'
-    df = read_file(url,True)
+    df = read_file(url, True)
     calculate_distance(df)
+    # Show only users who can be invited
     df = df[df.invite == 'yes']
+    # Sort the dataframe using user_id
     df = df.sort_values(['user_id'], ascending=True)
-    print(df.loc[:, 'name':'user_id'])
+    # Print only name and user_id
+    df = df.loc[:, 'name':'user_id']
+    # Pretty-print Output using Tabulate
+    print(tabulate(df, headers='keys', tablefmt='psql', showindex=False))
 
 if __name__== "__main__":
     main()
